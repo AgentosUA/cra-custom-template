6915cc9f0793a5de7bc597efabd5f813ddaa38a4
AgentosUA <agentoswork@gmail.com>
2022-05-26 22:33:05 +0300

    add sample & fix typo

--- src/store/general/actions.ts
+++ src/store/general/actions.ts
@@ -2,9 +2,6 @@ import { make } from 'redux-chill';
 /**
  * Startup application
  */
-const startup = make('[general] startup')
-  .stage('')
-  .stage('success')
-  .stage('failure');
+const startup = make('[general] startup').stage('success').stage('failure');
 
 export { startup };
